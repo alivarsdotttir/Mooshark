d93dba53d7f7df92a003cb0161c978df014404f8
alivarsdotttir <alexandralif@hotmail.com>
2016-04-30 16:14:06 +0000

    initial push

---
+++ Mooshark/Mooshark/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshark
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
