7813e0fc23f2840a243267fa5d9db226b461b4f1
juliaodds18 <juliao15@ru.is>
2016-05-01 19:50:50 +0000

    Set up tables for database. Still does not work, though, but hopefully tomorrow. :)

--- Mooshark/Mooshark/Controllers/HomeController.cs
+++ Mooshark/Mooshark/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshark.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace Mooshark.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext(); 
         public ActionResult Index()
         {
             return View();
